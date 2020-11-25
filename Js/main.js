62c4a781b8105417bc821bbf91ddd1c7b21a6cbb
顾风 <51695264+q4w5e6@users.noreply.github.com>
2020-11-25 17:52:26 +0800

    可下载文件

---
+++ Js/main.js
@@ -0,0 +1,19 @@
+
+    var n=0;
+    function Docopy() {
+    alert("复制就好啦＞︿＜");
+    }
+    function Lsp() {
+    n++;
+        alert("不要乱点啊喂 (*////▽////*)\n\n呐~\n图给你(*^▽^*)");
+        if(n=1){
+    Genshin_Impact()
+            n=0;
+        }
+    }
+    function Genshin_Impact(){
+    let a = document.createElement('a');
+        a.href = 'Images/Genshin Impact.png';
+        a.download = 'Genshin Impact.png';
+        a.click();
+    }
