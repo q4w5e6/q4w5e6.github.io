5b1de156f839ff58689d961b22536a4890544029
顾风 <51695264+q4w5e6@users.noreply.github.com>
2020-11-25 19:03:01 +0800

    Create main.js

--- Js/main.js
+++ Js/main.js
@@ -1,7 +1,15 @@
 
     var n=0;
     function Docopy() {
-    alert("复制就好啦＞︿＜");
+       var Url2=document.getElementById("url").innerText;
+        var oInput = document.createElement('input');
+        oInput.value = Url2;
+        document.body.appendChild(oInput);
+        oInput.select(); // 选择对象
+        document.execCommand("Copy"); // 执行浏览器复制命令
+        oInput.className = 'oInput';
+        oInput.style.display='none';
+        alert('复制成功＞︿＜');
     }
     function Lsp() {
     n++;
