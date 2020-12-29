d5cd0ebe05b86bf464d6add31c2fb3b3566bd0ef
顾风 <51695264+q4w5e6@users.noreply.github.com>
2020-12-29 22:26:59 +0800

    Update main.js

--- Js/main.js
+++ Js/main.js
@@ -77,7 +77,7 @@
             var isIE = !!window.ActiveXObject || "ActiveXObject" in window;
             var isFirefox = ua.indexOf("Firefox") > -1;
             var isEdge = ua.indexOf('Edge') > -1;
-            var isOpera = ua.indexOf('opr') > -1;
+            var isOpera = ua.indexOf('opr') > -1 && window.opr;
             var isChrome = ua.indexOf("Chrome") > -1 && window.chrome;
             var isSafari = ua.indexOf("Safari") > -1 && (ua.indexOf("like Mac OS X") > -1 || ua.indexOf("Macintosh") > -1) ;
             var isAndroid = ua.indexOf("Andriod") > -1;
