afb198ac38e7b8d7b4a5fa8afa36aa90c1e54690
顾风 <51695264+q4w5e6@users.noreply.github.com>
2020-12-29 16:09:25 +0800

    Update main.js

--- Js/main.js
+++ Js/main.js
@@ -22,12 +22,12 @@
         if(n=1){
             alert("不要乱点啊喂 (*////▽////*)\n\n呐~\n图给你(*^▽^*)");
             //Genshin_Impact();
-            downloadFile('https://cdn.jsdelivr.net/gh/q4w5e6/q4w5e6.github.io@master/Images/Genshin%20Impact.jpg', 'Genshin Impact.jpg');
+            downloadFile('cdn.jsdelivr.net/gh/q4w5e6/q4w5e6.github.io@master/Images/Genshin%20Impact.jpg', 'Genshin Impact.jpg');
         }
     }
     function downloadFile(url, fileName) {//跨域文件路径、下载到本地的文件名
     url = url.replace(/\\/g, '/');
-    const x = new XMLHttpRequest();
+    var x = new XMLHttpRequest();
     x.open("GET", url, true);
     x.responseType = 'blob';
     x.onload = function (e) {
@@ -47,11 +47,11 @@
     function clearCache(){
         var clearcache = new XMLHttpRequest();
         clearcache.Response.AddHeader("Access-Control-Allow-Origin", "*");
-        clearcache.open("GET", 'https://purge.jsdelivr.net/gh/q4w5e6/q4w5e6.github.io@master/File/unblockMusic.yaml', true);
+        clearcache.open("GET", 'purge.jsdelivr.net/gh/q4w5e6/q4w5e6.github.io@master/File/unblockMusic.yaml', true);
         clearcache.send();
     }
     function unlockmusicConfig(){
-        downloadFile('https://cdn.jsdelivr.net/gh/q4w5e6/q4w5e6.github.io@master/File/unblockMusic.yaml', '解锁网易云配置.yaml');
+        downloadFile('cdn.jsdelivr.net/gh/q4w5e6/q4w5e6.github.io@master/File/unblockMusic.yaml', '解锁网易云配置.yaml');
     }
     /*function Genshin_Impact(){
     let a = document.createElement('a');
